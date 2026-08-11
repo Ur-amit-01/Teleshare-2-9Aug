4ddb8fb247c5ac16db4895e6d1f5f06e65dba89d
Amit <heroku467@gmail.com>
2026-08-11 09:00:07 +0530

    Update start.py

--- plugins/filestore/start.py
+++ plugins/filestore/start.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 
 from plugins.helper.db import db
 from plugins.helper.settings import settings
-from plugins.helper.filters import ensure_subscribed
+from plugins.helper.force_sub import ensure_subscribed
 from plugins.filestore.delivery import deliver
 
 
@@ -21,16 +21,4 @@ async def start(client, message: Message):
 
     text = settings.get("start_text").format(mention=message.from_user.mention)
     await message.reply_text(text)
-
-
-# Track "request to join" submissions for force-sub channels that use
-# join-request mode, so ensure_subscribed() can treat them as satisfied.
-@Client.on_chat_join_request()
-async def track_join_request(client, chat_join_request):
-    channel_ids = {
-        ch if isinstance(ch, int) else None
-        for ch in settings.get("force_sub_channels")
-    }
-    chat_id = chat_join_request.chat.id
-    if chat_id in channel_ids or chat_join_request.chat.username in settings.get("force_sub_channels"):
-        await db.record_join_request(chat_join_request.from_user.id, chat_id)
+    
